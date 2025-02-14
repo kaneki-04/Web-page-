13006638406148e2ae2599d9ae14da800479abbb
Kevin Alejandro <kvin121204@gmail.com>
2025-02-14 10:17:28 -0600

    cambioss

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const mensaje = document.querySelector("p");
     mensaje.style.opacity = "0";
+    mensaje.style.transform = "translateY(10px)";
     setTimeout(() => {
-        mensaje.style.transition = "opacity 2s";
+        mensaje.style.transition = "opacity 1.5s, transform 1.5s";
         mensaje.style.opacity = "1";
+        mensaje.style.transform = "translateY(0)";
     }, 500);
 });
